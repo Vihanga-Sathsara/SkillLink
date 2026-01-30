cfddbdeae479bbd8d097ba14fe70851bf0696943
Hewa Pathage Vihanga Sathsara <sathsaravihanga27@gmail.com>
2026-01-30 13:13:23 +0530

    Change login and register ui features

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -2,7 +2,7 @@ import { loginUser } from "@/service/authService"
 import { router } from "expo-router"
 import React, { useEffect } from "react"
 
-import { View, Text, Dimensions, Pressable, TextInput, TouchableWithoutFeedback, Keyboard } from "react-native"
+import { View, Text, Dimensions, Pressable, TextInput, TouchableWithoutFeedback, Keyboard, ScrollView, KeyboardAvoidingView, Platform } from "react-native"
 
 const handleNavigate = () => {
     router.replace("/register")
@@ -41,27 +41,31 @@ const Login = () => {
       const {width , height} =  Dimensions.get('window')
     return (
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={{ width:width, height:height, alignItems:"center", justifyContent:"center", backgroundColor:"#F9FAFB"}}>
-          <View className="w-[80%] h-[90%] justify-center">
-            <Text className="text-[#111827] text-3xl text-center font-bold mb-4">Welcome Back to SkillLink</Text>
-            <Text className="text-center font-semibold text-gray-500 mb-5">Login to access your account and connect with professionals or clients instantly.</Text>
-            <Text className="text-2xl font-bold mb-4">Login</Text>
-            <View className="w-full items-center">
-              <Pressable className="w-full items-center  bg-white border border-gray-300 p-3 rounded-md mb-4">
-                <Text>Google Login</Text>
-              </Pressable>
-            </View>
-            <Text className="text-center mb-4">or login with credentials</Text>
-            <View className="w-full">
-              <TextInput placeholder="Email" className="w-full p-3 border rounded-md mb-4" value={email} onChangeText={setEmail}></TextInput>
-              <TextInput placeholder="Password" className="w-full p-3 border rounded-md mb-4" value={password} onChangeText={setPassword} secureTextEntry></TextInput>
-              <Text className="mb-4">If you don't have an account, please <Pressable onPress={handleNavigate}><Text className="text-blue-500">register</Text></Pressable></Text>
-              <Pressable className="w-full items-center bg-[#1E40AF] p-3 rounded-md mb-4" onPress={() => loginUserWithEmailAndPassword(email, password)}>
-                <Text className="text-white font-bold">Login</Text>
-              </Pressable>
-            </View>
-          </View>
-        </View>
+         <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{ flex: 1 }}>
+              <ScrollView bounces={false} showsVerticalScrollIndicator={false}>
+                  <View style={{ width:width, height:height, alignItems:"center", justifyContent:"center", backgroundColor:"#F9FAFB"}}>
+                    <View className="w-[80%] h-[90%] justify-center">
+                      <Text className="text-[#111827] text-3xl text-center font-bold mb-4">Welcome Back to SkillLink</Text>
+                      <Text className="text-center font-semibold text-gray-500 mb-5">Login to access your account and connect with professionals or clients instantly.</Text>
+                      <Text className="text-2xl font-bold mb-4">Login</Text>
+                      <View className="w-full items-center">
+                        <Pressable className="w-full items-center  bg-white border border-gray-300 p-3 rounded-md mb-4">
+                          <Text>Google Login</Text>
+                        </Pressable>
+                      </View>
+                      <Text className="text-center mb-4">or login with credentials</Text>
+                      <View className="w-full">
+                        <TextInput placeholder="Email" className="w-full p-3 border rounded-md mb-4" value={email} onChangeText={setEmail}></TextInput>
+                        <TextInput placeholder="Password" className="w-full p-3 border rounded-md mb-4" value={password} onChangeText={setPassword} secureTextEntry></TextInput>
+                        <Text className="mb-4">If you don't have an account, please <Pressable onPress={handleNavigate}><Text className="text-blue-500">register</Text></Pressable></Text>
+                        <Pressable className="w-full items-center bg-[#1E40AF] p-3 rounded-md mb-4" onPress={() => loginUserWithEmailAndPassword(email, password)}>
+                          <Text className="text-white font-bold">Login</Text>
+                        </Pressable>
+                      </View>
+                    </View>
+                  </View>
+              </ScrollView>
+        </KeyboardAvoidingView>
       </TouchableWithoutFeedback>    
     )
 }
--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,6 +1,6 @@
 import { router } from "expo-router"
 import React from "react"
-import { View, Text, Dimensions, TextInput, Pressable, TouchableWithoutFeedback, Keyboard, TouchableOpacity, Alert } from "react-native"
+import { View, Text, Dimensions, TextInput, Pressable, TouchableWithoutFeedback, Keyboard, TouchableOpacity, KeyboardAvoidingView, ScrollView, Platform } from "react-native"
 import { useLoader } from "@/hooks/useLoader"
 import { registerUser } from "@/service/authService"
 
@@ -48,41 +48,44 @@ const Register = () => {
         }
     }
 
-    
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={{ width:width, height:height, alignItems:"center", justifyContent:"center", backgroundColor:"#F9FAFB"}}>
-            <View className="w-[80%] h-[90%] justify-center">
-                <Text className="text-[#111827] text-3xl text-center font-bold mb-4">Welcome to SkillLink</Text>
-                <Text className="text-center font-semibold text-gray-500 mb-5">Join SkillLink today to hire trusted freelancers or showcase your skills to get hired.</Text>
-                <Text className="text-2xl font-bold mb-4">Register</Text>
-                <View className="w-full flex-row justify-between">
-                    <TouchableOpacity onPress={() => setRole("client")} className={`p-4 rounded-md mb-4 border w-[48%] ${role === "client"? "border-[#4651e5] bg-[#E0E7FF]": "border-gray-300 bg-white"}`}>
-                        <Text>Hire Freelancers</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => setRole("freelancer")} className={`p-4 rounded-md mb-4 border w-[48%] ${role === "freelancer"? "border-[#4651e5] bg-[#E0E7FF]": "border-gray-300 bg-white"}`}>
-                        <Text>Work as Freelancer</Text>
-                    </TouchableOpacity>
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{ flex: 1 }}>
+            <ScrollView bounces={false} showsVerticalScrollIndicator={false}>
+                <View style={{ width:width, height:height, alignItems:"center", justifyContent:"center", backgroundColor:"#F9FAFB"}}>
+                    <View className="w-[80%] h-[80%] justify-center">
+                        <Text className="text-[#111827] text-3xl text-center font-bold mb-4">Welcome to SkillLink</Text>
+                        <Text className="text-center font-semibold text-gray-500 mb-5">Join SkillLink today to hire trusted freelancers or showcase your skills to get hired.</Text>
+                        <Text className="text-2xl font-bold mb-4">Register</Text>
+                        <View className="w-full flex-row justify-between">
+                            <TouchableOpacity onPress={() => setRole("client")} className={`p-4 rounded-md mb-4 border w-[48%] ${role === "client"? "border-[#4651e5] bg-[#E0E7FF]": "border-gray-300 bg-white"}`}>
+                                <Text>Hire Freelancers</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => setRole("freelancer")} className={`p-4 rounded-md mb-4 border w-[48%] ${role === "freelancer"? "border-[#4651e5] bg-[#E0E7FF]": "border-gray-300 bg-white"}`}>
+                                <Text>Work as Freelancer</Text>
+                            </TouchableOpacity>
+                        </View>
+                        
+                        <View className="w-full items-center">
+                            <Pressable className="w-full items-center bg-white border border-gray-300 p-3 rounded-md mb-4">
+                                <Text>Google Register</Text>
+                            </Pressable>
+                        </View>
+                        <Text className="text-center mb-4">or register with credentials</Text>
+                        <View className="w-full">
+                            <TextInput placeholder="Full Name" className="w-full p-3 border rounded-md mb-4" value={fullName} onChangeText={setFullName}></TextInput>
+                            <TextInput placeholder="Email" className="w-full p-3 border rounded-md mb-4" value={email} onChangeText={setEmail}></TextInput>
+                            <TextInput placeholder="Password" className="w-full p-3 border rounded-md mb-4" value={password} onChangeText={setPassword} secureTextEntry></TextInput>
+                            <TextInput placeholder="Confirm Password" className="w-full p-3 border rounded-md mb-4" value={confirmPassword} onChangeText={setConfirmPassword} secureTextEntry></TextInput>
+                            <Text className="mb-4">Already have an account? <Pressable onPress={handleNavigate}><Text className="text-blue-500">Login</Text></Pressable></Text>
+                            <Pressable className="w-full bg-[#1E40AF] p-3 rounded-md mb-4" onPress={registerNewUser}>
+                                <Text className="text-center text-white font-bold">Register</Text>
+                            </Pressable>
+                        </View>
+                    </View>
                 </View>
-                
-                <View className="w-full items-center">
-                    <Pressable className="w-full items-center bg-white border border-gray-300 p-3 rounded-md mb-4">
-                        <Text>Google Register</Text>
-                    </Pressable>
-                </View>
-                <Text className="text-center mb-4">or register with credentials</Text>
-                <View className="w-full">
-                    <TextInput placeholder="Full Name" className="w-full p-3 border rounded-md mb-4" value={fullName} onChangeText={setFullName}></TextInput>
-                    <TextInput placeholder="Email" className="w-full p-3 border rounded-md mb-4" value={email} onChangeText={setEmail}></TextInput>
-                    <TextInput placeholder="Password" className="w-full p-3 border rounded-md mb-4" value={password} onChangeText={setPassword} secureTextEntry></TextInput>
-                    <TextInput placeholder="Confirm Password" className="w-full p-3 border rounded-md mb-4" value={confirmPassword} onChangeText={setConfirmPassword} secureTextEntry></TextInput>
-                    <Text className="mb-4">Already have an account? <Pressable onPress={handleNavigate}><Text className="text-blue-500">Login</Text></Pressable></Text>
-                    <Pressable className="w-full bg-[#1E40AF] p-3 rounded-md mb-4" onPress={registerNewUser}>
-                        <Text className="text-center text-white font-bold">Register</Text>
-                    </Pressable>
-                </View>
-            </View>
-        </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     </TouchableWithoutFeedback>
   )
 }
